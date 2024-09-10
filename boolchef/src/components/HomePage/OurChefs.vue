f425b01dacb4e553c2b681404bcb9d495e37f9db
SimoneColombo1 <simcolombo2@gmail.com>
2024-09-10 17:31:24 +0200

    fatta la struttura del carosello

--- boolchef/src/components/HomePage/OurChefs.vue
+++ boolchef/src/components/HomePage/OurChefs.vue
@@ -42,9 +42,28 @@ export default {
           cognome: "Andrés",
           specialità: "Cucina spagnola e molecolare",
         },
+        {
+          nome: "Nobu",
+          cognome: "Matsuhisa",
+          specialità: "Cucina giapponese fusion",
+        },
+        { nome: "Alain", cognome: "Ducasse", specialità: "Cucina francese" },
+        { nome: "Carlo", cognome: "Cracco", specialità: "Cucina italiana" },
+        {
+          nome: "René",
+          cognome: "Redzepi",
+          specialità: "Cucina nordica e molecolare",
+        },
+        { nome: "Anne-Sophie", cognome: "Pic", specialità: "Cucina francese" },
+        {
+          nome: "José",
+          cognome: "Andrés",
+          specialità: "Cucina spagnola e molecolare",
+        },
       ],
     };
   },
+
   methods: {
     scrollLeft() {
       this.indexScroll--;
@@ -53,13 +72,13 @@ export default {
         console.log(this.indexScroll);
       } else {
         this.$refs.scrollable.scrollBy({ left: 3500, behavior: "smooth" });
-        this.indexScroll = this.projects.length - 2;
+        this.indexScroll = this.Chefs.length - 2;
         console.log(this.indexScroll);
       }
     },
     scrollRight() {
       this.indexScroll++;
-      if (this.indexScroll < this.projects.length - 2) {
+      if (this.indexScroll < this.Chefs.length - 2) {
         this.$refs.scrollable.scrollBy({ left: 478, behavior: "smooth" });
 
         console.log(this.indexScroll);
@@ -74,29 +93,37 @@ export default {
 </script>
 
 <template>
-  <section>
-    <p @click="scrollLeft()" class="left">></p>
-    <div class="cards-container">
-      <div v-for="chef in Chefs">
-        <span>{{ chef.nome }}</span>
-        <span> {{ chef.cognome }} </span>
-        <span> {{ chef.specialità }} </span>
-      </div>
+  <h1>i nostri chef</h1>
+  <a @click="scrollLeft()" class="left"> s</a>
+  <a @click="scrollRight()" class="right"> > </a>
+  <div class="chefs-container" ref="scrollable">
+    <div v-for="chef in Chefs" class="single-chef">
+      {{ chef.nome }}
+      {{ chef.cognome }}
+      {{ chef.specializazione }}
     </div>
-    <a @click="scrollRight()" class="right"> < </a>
-  </section>
+  </div>
 </template>
 
-<style scoped>
-.cards-container {
+<style scoped lang="scss">
+.chefs-container {
+  overflow: hidden;
   display: flex;
   flex-direction: row;
-  justify-content: center;
-  overflow: hidden;
-  div {
-    width: 10rem;
+  width: 100%;
+  flex-wrap: nowrap;
+  .single-chef {
+    display: flex;
+    flex-direction: column;
     height: 20rem;
-    margin: 10rem;
+    width: 100%;
+    min-width: 10rem;
   }
+}
+.left {
+  display: flex;
+  position: absolute;
+  top: 30;
+  right: 60rem;
 }
 </style>
