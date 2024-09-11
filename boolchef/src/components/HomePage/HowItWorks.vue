b2dfdc196a161158711f9a0aa70ffcc4fe6177d5
SimoneColombo1 <simcolombo2@gmail.com>
2024-09-11 11:34:37 +0200

    Installato axios

--- boolchef/src/components/HomePage/HowItWorks.vue
+++ boolchef/src/components/HomePage/HowItWorks.vue
@@ -1,47 +1,42 @@
 <script>
 export default {
-    data() {
-        return {
-            items: [{ message: 'Chatta' }, { message: 'Prenota' },
-                    { message: 'Personalizza' }, { message: 'Menu' },
-                    { message: 'Esperinza' }, { message: 'Trova il tuo chef' }
-            ]
-        }
-    }
-}
-
+  data() {
+    return {
+      items: [
+        { message: "Chatta" },
+        { message: "Prenota" },
+        { message: "Personalizza" },
+        { message: "Menu" },
+        { message: "Esperinza" },
+        { message: "Trova il tuo chef" },
+      ],
+    };
+  },
+};
 </script>
 
-<<<<<<< HEAD
 <template>
-    <div class="container">
-        <div class="row text-center mt-5">
-            <h2>
-                Come Funziona
-            </h2>
-        </div>
-        <div class="row">
-            <div class="col-12 d-flex justify-content-evenly flex-wrap ">
-                <div v-for="item in items" class="card misure text-center">
-                    <div class="card-body">
-                        {{ item.message }}
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row text-center mt-5">
+      <h2>Come Funziona</h2>
     </div>
+    <div class="row">
+      <div class="col-12 d-flex justify-content-evenly flex-wrap">
+        <div v-for="item in items" class="card misure text-center">
+          <div class="card-body">
+            {{ item.message }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-    .misure {
-        padding: 3rem;
-        width: calc((100% /3) - 3rem);
-        margin-top: 3rem;
-        margin-bottom: 3rem;
-
-
-    }
+.misure {
+  padding: 3rem;
+  width: calc((100% / 3) - 3rem);
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+}
 </style>
-=======
-<style scoped lang="scss"></style>
->>>>>>> 70334c67bfe4009761b88710bbcd1fde20919b4d
