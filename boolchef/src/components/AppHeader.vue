fdc0cc7eac69dc74cc4ea42f31298f53a9096695
VitoMarino <marino.vito2001@gmail.com>
2024-09-11 16:28:26 +0200

    header

--- boolchef/src/components/AppHeader.vue
+++ boolchef/src/components/AppHeader.vue
@@ -1,30 +1,52 @@
-<script></script>
+<script>
+export default {
+    data() {
+        return {
+            navLinkNames: [
+                {
+                    label: 'Homepage',
+                    name: 'home'
+                },
+                {
+                    label: 'Trova il tuo chef',
+                    name: 'search-chef'
+                },
+            ]
+        }
+    }
+}
+</script>
 
 <template>
-  <header>
-    <nav>
-      <span>LOGO</span>
-      <ul>
-        <li>Home</li>
-        <li>Trova il tuo Chef</li>
-      </ul>
-    </nav>
-  </header>
+    <header>
+        <nav>
+            <span>LOGO</span>
+            <ul>
+                <li class="ms-3" v-for="navItem in navLinkNames">
+                    <router-link :to="{ name: navItem.name }">
+                        {{ navItem.label }}
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
 <style lang="scss" scoped>
 header {
-  display: flex;
-  max-width: 1400px;
-  margin: 0 auto;
-  nav {
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    ul {
-      list-style: none;
-      display: flex;
-      flex-direction: row;
+    max-width: 1400px;
+    margin: 0 auto;
+
+    nav {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+        ul {
+            list-style: none;
+            display: flex;
+            flex-direction: row;
+        }
     }
-  }
 }
 </style>
