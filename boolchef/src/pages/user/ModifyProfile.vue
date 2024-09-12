be68024e52b89d09d0463e33e9844df9e61b1314
LorezoParis1996 <lorenzoparis86@live.it>
2024-09-12 12:14:19 +0200

    modificato form

--- boolchef/src/pages/user/ModifyProfile.vue
+++ boolchef/src/pages/user/ModifyProfile.vue
@@ -18,24 +18,24 @@ export default {
         <h3 class="mb-3">Modifica profilo</h3>
         <form>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Indirizzo</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="address" class="form-label">Indirizzo</label>
+                <input type="text" class="form-control" id="address">
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">CV</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="CV" class="form-label">Curriculum Vitae</label>
+                <input type="file" class="form-control" id="CV">
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Foto profilo</label>
-                <input type="file" class="form-control" id="exampleInputPassword1">
+                <label for="photograph" class="form-label">Foto profilo</label>
+                <input type="file" class="form-control" id="photograph">
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Telefono</label>
-                <input type="file" class="form-control" id="exampleInputPassword1">
+                <label for="telephone" class="form-label">Telefono</label>
+                <input type="text" class="form-control" id="telephone">
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Descrizione piatti</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="description_of_dishes" class="form-label">Descrizione piatti</label>
+                <input type="password" class="form-control" id="description_of_dishes">
             </div>
             <div class="mb-3">
                 <select class="form-select" aria-label="Default select example">
