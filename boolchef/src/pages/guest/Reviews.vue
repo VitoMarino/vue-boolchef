e899dcf32387af7c5f1860b2b1fef63ce0c15103
SimoneColombo1 <simcolombo2@gmail.com>
2024-09-16 19:45:28 +0200

    Grafica del form per l'invio di una recensione

--- boolchef/src/pages/guest/Reviews.vue
+++ boolchef/src/pages/guest/Reviews.vue
@@ -9,17 +9,18 @@ export default {
       user_name: '',
       email: '',
       chef_id: 1,
+      chefs:[]
     };
   },
   methods: {
-    postMessage(){
+    postMessage(id){
         axios
         .post("http://127.0.0.1:8000/api/reviews", {
             review_title: this.review_title,
             review: this.review,
             user_name: this.user_name,
             email: this.email,
-            chef_id: this.chef_id,
+            chef_id: id,
         })
         .then((response) => {
           console.log(response.data.results);
@@ -29,6 +30,8 @@ export default {
           console.log(error);
         });
     },
+    
+   
   },
   created() {
     
@@ -41,32 +44,71 @@ export default {
 
 
 <template>
-    <form @submit.prevent="postMessage">
+   <main>
+    <section>
+        <h1>Scrivi la tua recensione</h1>
+   <form @submit.prevent="postMessage" class="form-group card">
     <!-- Altri campi del messaggio -->
     
     <input type="hidden" name="chef_id" value="{{ $chef->id }}"> <!-- Campo nascosto con l'ID dello chef -->
 
     <label for="review_title">Titolo:</label>
-    <input type="text" name="review_title" id="review_title" v-model="review_title" required>
+    <input type="text" name="review_title" id="review_title" v-model="review_title" required class="form-control">
 
     <label for="user_name">Nome da presentare:</label>
-    <input type="text" name="user_name" id="user_name" v-model="user_name" required>
+    <input type="text" name="user_name" id="user_name" v-model="user_name" required class="form-control">
 
     <label for="email">Email per la risposta:</label>
-    <input type="text" name="email" id="email" v-model="email" required>
+    <input type="text" name="email" id="email" v-model="email" required class="form-control">
 
     <label for="review">Recensione</label>
-    <textarea name="review" id="review" v-model="review" required></textarea>
+    <textarea name="review" id="review" v-model="review" required class="form-control"></textarea>
     
 
 
 
     <button type="submit">Invia Recensione</button>
 </form>
-
-
+</section>
+</main>
 
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+section{
+    max-width: 1400px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
+   form{
+    padding: 4rem;
+    margin: 6rem;
+    width: 80rem;
+    input{
+        margin-bottom: 0.6rem;
+    }
+    button{
+        margin-top: 1rem;
+        width:10rem;
+        padding: 0.3rem;
+        border-radius: 1rem;
+        background-color: goldenrod;
+        border: 1px solid goldenrod;
+        &:hover{
+            background-color: white;
+           
+            width: 11rem;
+        }
+    }
+   }
+    
+}
+
+
+
+</style>
--- boolchef/src/pages/guest/SingleChef.vue
+++ boolchef/src/pages/guest/SingleChef.vue
@@ -56,8 +56,8 @@ export default {
         </span>
       {{ chef.reviews.length }}
   <span>
-    Hai gia provato questo chef? Allora scrivi una recensione
-    <RouterLink  :to="{name:'reviews', params:{ id: chef.id }}"><button>ddd</button></RouterLink>
+    Hai gia provato questo chef? 
+    <RouterLink  :to="{name:'reviews', params:{ id: chef.id }}"><button>Scrivi una recensione</button></RouterLink>
   </span>
     <span>
     Contatta lo chef
@@ -66,6 +66,7 @@ export default {
 </div>
     </section>
 </main>
+
 
 
 </template>
